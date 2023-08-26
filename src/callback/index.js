5c11686e84046f4fbd3e979f1abf786108f86b6a
ECastro08 <worldmechatronics2023@gmail.com>
2023-08-25 22:38:01 -0500

    iniciamos con las callbacks y creamos el archivo index.js donde se utilizo el concepto de callback

---
+++ src/callback/index.js
@@ -0,0 +1,25 @@
+// CALLBACKS 
+// creamos la funcion la cual vamos a llamar mas adelante
+function sum(num1, num2) {
+    return num1 + num2;
+  }
+  
+  function calc(num1, num2, sumNumbers) {
+    return sumNumbers(num1, num2);
+  };
+  
+  console.log(calc(2, 2, sum));
+
+  // ejemplo con setTimeout en una funcion anonima
+  
+  setTimeout(function () {
+    console.log('Hola JavaScript');
+  }, 5000)
+  
+  // ejemplo con setTimeout y una funcion de saludo 
+
+  function gretting(name) {
+    console.log(`Hola ${name}`);
+  }
+  
+  setTimeout(gretting, 2000, 'Oscar');
